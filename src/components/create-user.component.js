fed4a64b973f7ddf269d28f17edff6ace92dd123
Karan Dua <karandua2002@gmail.com>
2021-10-19 23:29:04 +0530

    added file upload

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,57 +1,161 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
 export default class CreateUser extends Component {
 
     constructor(props) {
         super(props);
-        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeSellername = this.onChangeSellername.bind(this);
+        this.onChangeContact = this.onChangeContact.bind(this);
+        this.onChangeAddress = this.onChangeAddress.bind(this);
+        this.fileSelectedHandler = this.fileSelectedHandler.bind(this);
+        this.onChangeBusinessname = this.onChangeBusinessname.bind(this);
+        this.onChangeBusinessdesc = this.onChangeBusinessdesc.bind(this);
+        this.onChangeDelivery = this.onChangeDelivery.bind(this);
+
         this.onSubmit = this.onSubmit.bind(this);
         this.state = {
-          username: ''
+          sellername: '',
+          contact: '',
+          address: '',
+          profilepic: '',
+          businessname: '',
+          businessdesc: '',
+          delivery: '',
         };
+      };
+
+      onChangeSellername(e) {
+        this.setState({
+          sellername: e.target.value
+        });
+      };
+
+      onChangeContact(e) {
+        this.setState({
+          contact: e.target.value
+        });
+      };
+
+      onChangeAddress(e) {
+        this.setState({
+          address: e.target.value
+        });
+      };
+
+      fileSelectedHandler(e) {
+        this.setState({
+          profilepic : e.target.files[0]
+        })
       }
 
-      onChangeUsername(e) {
+      onChangeBusinessname(e) {
         this.setState({
-          username: e.target.value
+          businessname: e.target.value
         });
-      }
+      };
+
+      onChangeBusinessdesc(e) {
+        this.setState({
+          businessdesc: e.target.value
+        });
+      };
+
+      onChangeDelivery(e) {
+        this.setState({
+          delivery: e.target.value
+        });
+      };
+
       onSubmit(e) {
         e.preventDefault();
-        const newUser = {
-          username: this.state.username,
-        };
-        console.log(newUser);
-        axios.post('https://karandua.herokuapp.com/users/add', newUser)
+        const newSeller = new FormData();
+        newSeller.append("sellername", this.state.sellername);
+        newSeller.append("contact", this.state.contact);
+        newSeller.append("address", this.state.address);
+        newSeller.append("profilepic", this.state.profilepic);
+        newSeller.append("businessname", this.state.businessname);
+        newSeller.append("businessdesc", this.state.businessdesc);
+        newSeller.append("delivery", this.state.delivery);
+        
+        axios.post('https://karanmahesh.herokuapp.com/sellers/add', newSeller)
         .then(res => console.log(res.data));
         
-        alert(`User ${this.state.username} added succesfully`);
+        alert(`Seller ${this.state.sellername} added succesfully`);
 
         this.setState({
-          username: ''
+          sellername: ''
         })
 
         
-      }
+      };
 
     render() {
         return (
             <div>
-            <h3>Create New User</h3>
+            <h3>Create New Seller</h3>
             <form onSubmit={this.onSubmit}>
               <div className="form-group"> 
-                <label>Username: </label>
+                <label>Seller name: </label>
                 <input  type="text"
                     required
                     className="form-control"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
+                    value={this.state.sellername}
+                    onChange={this.onChangeSellername}
                     />
               </div>
+              <div className="form-group"> 
+                <label>Contact: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.contact}
+                    onChange={this.onChangeContact}
+                    />
+              </div>
+              <div className="form-group"> 
+                <label>Address: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.address}
+                    onChange={this.onChangeAddress}
+                    />
+              </div>
+              <div className="form-group"> 
+                <label>Profile Pic / Business Pic: </label>
+                <input type="file" onChange={this.fileSelectedHandler}/>
+              </div>
+              <div className="form-group"> 
+                <label>Business name: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.businessname}
+                    onChange={this.onChangeBusinessname}
+                    />
+              </div>
+              <div className="form-group"> 
+                <label>Business Description: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.businessdesc}
+                    onChange={this.onChangeBusinessdesc}
+                    />
+              </div>
+              <div className="form-group"> 
+                <label>Delivery (true/false): </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.delivery}
+                    onChange={this.onChangeDelivery}
+                    />
+              </div>
+          
               <div className="form-group">
-                <input type="submit" value="Create User" className="btn btn-primary" />
+                <input type="submit" value="Create Seller" className="btn btn-primary" />
               </div>
             </form>
           </div>
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -9,7 +9,7 @@ const Exercise = props => (
     <td>{props.exercise.duration}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
     <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="/" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
     </td>
   </tr>
 )
