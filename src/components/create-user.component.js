b7c76ab218d33956ceb65ecdd152323f100a6fb7
Karan Dua <karandua2002@gmail.com>
2021-10-20 04:38:26 +0530

    finally hope cors work

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -78,7 +78,7 @@ export default class CreateUser extends Component {
         newSeller.append("businessdesc", this.state.businessdesc);
         newSeller.append("delivery", this.state.delivery);
         
-        axios.post('http://localhost:5000/sellers/add', newSeller)
+        axios.post('https://karanmahesh.herokuapp.com/sellers/add', newSeller)
         .then(res => console.log(res.data));
         
         alert(`Seller ${this.state.sellername} added succesfully`);
