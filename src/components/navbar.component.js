abe69f0a625e96eed939e57b55a05e927f26ecb1
Karan Dua <karandua2002@gmail.com>
2021-10-19 20:03:47 +0530

    Changing to add seller data

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,17 +6,23 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerciseTracker</Link>
+        <Link to="/" className="navbar-brand">Hi Mahesh</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">Seller_Data</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Add_Product</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
+          <Link to="/user" className="nav-link">Add_SubCategory</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Add_Category</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Add_Seller</Link>
           </li>
         </ul>
         </div>
